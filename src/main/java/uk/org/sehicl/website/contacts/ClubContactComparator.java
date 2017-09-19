488954a2359de4a9f044bbe487a66c1ed1ee28e0
Jeremy Hicks <jerhicks@cisco.com>
2017-09-19 16:40:34 +0100

    Refactor comparator

--- src/main/java/uk/org/sehicl/website/contacts/ClubContactComparator.java
+++ src/main/java/uk/org/sehicl/website/contacts/ClubContactComparator.java
@@ -3,6 +3,7 @@ package uk.org.sehicl.website.contacts;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 public class ClubContactComparator implements Comparator<ContactDetails>
 {
@@ -12,11 +13,17 @@ public class ClubContactComparator implements Comparator<ContactDetails>
     @Override
     public int compare(ContactDetails o1, ContactDetails o2)
     {
-        int answer = o1.getRole().getClub().compareTo(o2.getRole().getClub());
-        if (answer == 0)
-        {
-            answer = ROLES.indexOf(o1.getRole().getName()) - ROLES.indexOf(o2.getRole().getName());
-        }
+        int answer = compare(o1, o2, (o) -> o.getRole().getClub())
+                .andThen(compare(o1, o2, (o) -> ROLES.indexOf(o.getRole().getName()))
+                        .andThen(compare(o1, o2, (o) -> o
+                                .getName())))
+                .apply(0);
         return answer;
+    }
+
+    private <T, V extends Comparable<V>> Function<Integer, Integer> compare(T o1, T o2,
+            Function<T, V> dataGetter)
+    {
+        return (i) -> i != 0 ? i : dataGetter.apply(o1).compareTo(dataGetter.apply(o2));
     }
 }
