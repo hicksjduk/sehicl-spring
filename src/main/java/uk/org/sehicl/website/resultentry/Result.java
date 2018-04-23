afa85ed11ad0c1afa1466cfcbacfa20b4bc2b9fd
Jeremy Hicks <jerhicks@cisco.com>
2018-04-23 16:52:39 +0100

    Add Json unmarshalling

--- src/main/java/uk/org/sehicl/website/resultentry/Result.java
+++ src/main/java/uk/org/sehicl/website/resultentry/Result.java
@@ -1,12 +1,22 @@
 package uk.org.sehicl.website.resultentry;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import uk.org.sehicl.website.OrdinalDateFormatter;
 import uk.org.sehicl.website.data.League;
 import uk.org.sehicl.website.data.Match;
 import uk.org.sehicl.website.data.Model;
+import uk.org.sehicl.website.data.Player;
 import uk.org.sehicl.website.data.Team;
 
+@JsonPropertyOrder(
+{ "date", "time", "court", "leagueId", "leagueName", "homeTeam", "awayTeam" })
 public class Result
 {
     @SuppressWarnings("serial")
@@ -39,24 +49,48 @@ public class Result
         }
     }
 
-    private final Match match;
-    private final Team homeTeam;
-    private final Team awayTeam;
-
-    public Match getMatch()
+    public static class TeamData
     {
-        return match;
+        public final String id;
+        public final String name;
+        public final List<PlayerData> players;
+
+        public TeamData(Team t)
+        {
+            this.id = t.getId();
+            this.name = t.getName();
+            this.players = t
+                    .getPlayers()
+                    .stream()
+                    .sorted()
+                    .map(PlayerData::new)
+                    .collect(Collectors.toList());
+        }
     }
 
-    public Team getHomeTeam()
+    public static class PlayerData
     {
-        return homeTeam;
+        public final String id;
+        public final String name;
+
+        public PlayerData(Player p)
+        {
+            this.id = p.getId();
+            this.name = p.getName();
+        }
     }
 
-    public Team getAwayTeam()
-    {
-        return awayTeam;
-    }
+    private final static OrdinalDateFormatter DATE_FORMAT = new OrdinalDateFormatter(
+            new SimpleDateFormat("d MMMM yyyy"));
+    private final static DateFormat TIME_FORMAT = new SimpleDateFormat("h:mm");
+
+    public final String date;
+    public final String time;
+    public final String court;
+    public final String leagueId;
+    public final String leagueName;
+    public final TeamData homeTeam;
+    public final TeamData awayTeam;
 
     public Result(Model model, String leagueId, String homeTeamId, String awayTeamId)
             throws ResultException
@@ -64,13 +98,14 @@ public class Result
         final League league = model.getLeague(leagueId);
         if (league == null)
             throw ResultException.create("League %s not found", leagueId);
-        homeTeam = league.getTeam(homeTeamId);
-        if (homeTeam == null)
+        this.leagueId = league.getId();
+        leagueName = league.getName();
+        Team ht = league.getTeam(homeTeamId);
+        if (ht == null)
             throw ResultException.create("Team %s not found in league %s", homeTeamId, leagueId);
-        awayTeam = league.getTeam(awayTeamId);
-        if (awayTeam == null)
-            throw ResultException.create("Team %s not found in league %s", awayTeamId, leagueId);
-        match = league
+        homeTeam = getTeamData(league, homeTeamId);
+        awayTeam = getTeamData(league, awayTeamId);
+        Match match = league
                 .getMatches()
                 .stream()
                 .filter(m -> m.getHomeTeamId().equals(homeTeamId)
@@ -80,5 +115,16 @@ public class Result
         if (match == null)
             throw ResultException.create("Match between %s and %s not found in league %s",
                     homeTeamId, awayTeamId, leagueId);
+        date = DATE_FORMAT.format(match.getDateTime());
+        time = TIME_FORMAT.format(match.getDateTime());
+        court = match.getCourt();
+    }
+
+    private TeamData getTeamData(League l, String teamId) throws ResultException
+    {
+        Team t = l.getTeam(teamId);
+        if (t == null)
+            throw ResultException.create("Team %s not found in league %s", teamId, leagueId);
+        return new TeamData(t);
     }
 }
