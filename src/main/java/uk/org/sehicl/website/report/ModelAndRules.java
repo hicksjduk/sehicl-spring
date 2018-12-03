4b987ee1082baf572f563ae38def47c2ab5ac2d7
Jeremy Hicks <jerhicks@cisco.com>
2018-12-03 10:48:45 +0000

    New ModelAndRules class

---
+++ src/main/java/uk/org/sehicl/website/report/ModelAndRules.java
@@ -0,0 +1,22 @@
+package uk.org.sehicl.website.report;
+
+import uk.org.sehicl.website.data.Model;
+import uk.org.sehicl.website.dataload.ModelLoader;
+import uk.org.sehicl.website.rules.Rules;
+
+public class ModelAndRules
+{
+    public final Model model;
+    public final Rules rules;
+    
+    public ModelAndRules(int season)
+    {
+        this(ModelLoader.getModel(season), new Rules.Builder(season).build());
+    }
+
+    public ModelAndRules(Model model, Rules rules)
+    {
+        this.model = model;
+        this.rules = rules;
+    }
+}
