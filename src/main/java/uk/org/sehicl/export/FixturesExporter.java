a309f05739a021720fc2fd7e6c3e3138e0ad264b
Jeremy Hicks <jerhicks@cisco.com>
2019-02-11 09:57:26 +0000

    Refactoring

--- src/main/java/uk/org/sehicl/export/FixturesExporter.java
+++ src/main/java/uk/org/sehicl/export/FixturesExporter.java
@@ -1,23 +1,27 @@
 package uk.org.sehicl.export;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.stream.Stream;
 
 import uk.org.sehicl.website.data.League;
-import uk.org.sehicl.website.data.Match;
 import uk.org.sehicl.website.dataload.ModelLoader;
 
 public class FixturesExporter
 {
     public static void main(String[] args) throws Exception
     {
-        try (FileWriter w = new FileWriter(
-                "/Users/jerhicks/Documents/personal/sehicl/fixtures.csv"))
+        final File dir = new File(System.getProperty("user.home"), "Documents/personal/sehicl");
+        dir.mkdirs();
+        final File file = new File(dir, "fixtures.csv");
+        try (FileWriter w = new FileWriter(file))
         {
             new FixturesExporter().export(new PrintWriter(w));
         }
+        System.out.println("Output written to " + file.getAbsolutePath());
     }
 
     private static final DateFormat DATE_FORMATTER = new SimpleDateFormat("dd/MM/yyyy");
@@ -25,19 +29,15 @@ public class FixturesExporter
 
     private void export(PrintWriter pw) throws Exception
     {
-        ModelLoader
-                .getModel()
-                .getLeagues()
-                .stream()
-                .flatMap(l -> l.getMatches().stream().map(m -> matchData(l, m)))
-                .forEach(pw::println);
+        ModelLoader.getModel().getLeagues().stream().flatMap(this::matchData).forEach(pw::println);
     }
 
-    private String matchData(League l, Match m)
+    private Stream<String> matchData(League l)
     {
         final String divNum = l.getId().replaceAll("[^\\d]", "");
-        return String.format("%s,%s,%s,%s,%s,,,%s", DATE_FORMATTER.format(m.getDateTime()),
-                TIME_FORMATTER.format(m.getDateTime()), m.getCourt(), divNum,
-                l.getTeam(m.getHomeTeamId()).getName(), l.getTeam(m.getAwayTeamId()).getName());
+        return l.getMatches().stream().map(m -> String.format("%s,%s,%s,%s,%s,,,%s",
+                DATE_FORMATTER.format(m.getDateTime()), TIME_FORMATTER.format(m.getDateTime()),
+                m.getCourt(), divNum, l.getTeam(m.getHomeTeamId()).getName(),
+                l.getTeam(m.getAwayTeamId()).getName()));
     }
 }
