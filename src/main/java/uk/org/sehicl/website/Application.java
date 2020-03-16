d5d541be67fd09d644ad4510ca859be15fc16b32
Jeremy Hicks <jerhicks@synamedia.com>
2020-03-16 13:24:46 +0000

    Open local database if configured

--- src/main/java/uk/org/sehicl/website/Application.java
+++ src/main/java/uk/org/sehicl/website/Application.java
@@ -1,5 +1,6 @@
 package uk.org.sehicl.website;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,13 +8,14 @@ import org.springframework.context.annotation.Bean;
 import uk.org.sehicl.website.users.EmailSender;
 import uk.org.sehicl.website.users.UserDatastore;
 import uk.org.sehicl.website.users.UserManager;
+import uk.org.sehicl.website.users.impl.LocalDatabaseUserDatastore;
 import uk.org.sehicl.website.users.impl.RedisDatastore;
 import uk.org.sehicl.website.users.impl.SendgridSender;
 
 @SpringBootApplication
 public class Application
 {
-//    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+    // private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args)
     {
@@ -36,7 +38,10 @@ public class Application
     @Bean
     public UserDatastore userDatastore()
     {
-        return new RedisDatastore(System.getenv("REDIS_URL"));
+        String localDbAddr = System.getProperty("localdb");
+        if (StringUtils.isEmpty(localDbAddr))
+            return new RedisDatastore(System.getenv("REDIS_URL"));
+        return new LocalDatabaseUserDatastore(localDbAddr);
     }
 
     @Bean
