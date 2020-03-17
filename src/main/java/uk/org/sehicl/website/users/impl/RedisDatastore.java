43735ecde21e161fd2c0c7a23a5c7cd8e10a1170
Jeremy Hicks <jerhicks@synamedia.com>
2020-03-17 08:43:33 +0000

    Organise imports

--- src/main/java/uk/org/sehicl/website/users/impl/RedisDatastore.java
+++ src/main/java/uk/org/sehicl/website/users/impl/RedisDatastore.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
-import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
