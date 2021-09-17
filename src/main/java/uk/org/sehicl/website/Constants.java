e13414c4e2fe547b355fc5ad60c0245f1d2b939b
Jeremy Hicks <jerhicks@synamedia.com>
2021-09-17 16:37:52 +0100

    Changes for the new season

--- src/main/java/uk/org/sehicl/website/Constants.java
+++ src/main/java/uk/org/sehicl/website/Constants.java
@@ -3,5 +3,5 @@ package uk.org.sehicl.website;
 public interface Constants
 {
     public final static int FIRST_SEASON = 6;
-    public final static int CURRENT_SEASON = 20;
+    public final static int CURRENT_SEASON = 22;
 }
