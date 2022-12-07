51fd41151d94a082dc56d5de81005f65c1488711
Jeremy Hicks <jeremy@thehickses.org.uk>
2022-12-07 10:06:43 +0000

    Log error

--- src/main/java/uk/org/sehicl/admin/UsersImporter.java
+++ src/main/java/uk/org/sehicl/admin/UsersImporter.java
@@ -3,6 +3,9 @@ package uk.org.sehicl.admin;
 import java.io.BufferedReader;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.ArrayType;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -12,13 +15,15 @@ import uk.org.sehicl.website.users.UserDatastore;
 
 public class UsersImporter
 {
+    private final Logger LOG = LoggerFactory.getLogger(UsersImporter.class);
+
     private final UserDatastore datastore;
 
     public UsersImporter(UserDatastore datastore)
     {
         this.datastore = datastore;
     }
-    
+
     public int importUsers(BufferedReader data)
     {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
@@ -31,6 +36,7 @@ public class UsersImporter
         }
         catch (Exception e)
         {
+            LOG.error("Unable to import users", e);
             throw new RuntimeException(e);
         }
     }
