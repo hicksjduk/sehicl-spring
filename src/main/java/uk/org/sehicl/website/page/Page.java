2c5eb0fe91207eac36195796a4aaacf55a3ec63c
Jeremy Hicks <jeremy@thehickses.org.uk>
2022-12-08 11:30:45 +0000

    Block indexing if the BLOCK_INDEXING system property is set

--- src/main/java/uk/org/sehicl/website/page/Page.java
+++ src/main/java/uk/org/sehicl/website/page/Page.java
@@ -8,12 +8,14 @@ public abstract class Page
     private final String pageId;
     private final String contentTemplate;
     private final Navigator navigator;
+    private final boolean blockIndexing;
 
     public Page(String pageId, String contentTemplate, Section section, String uri)
     {
         this.pageId = pageId;
         this.contentTemplate = contentTemplate;
         this.navigator = new Navigator(section, uri);
+        this.blockIndexing = System.getenv("BLOCK_INDEXING") != null;
     }
 
     public abstract String getTitle();
@@ -31,5 +33,10 @@ public abstract class Page
     public Navigator getNavigator()
     {
         return navigator;
+    }
+
+    public boolean isBlockIndexing()
+    {
+        return blockIndexing;
     }
 }
